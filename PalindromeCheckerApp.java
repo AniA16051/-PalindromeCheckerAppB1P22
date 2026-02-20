d3bc293bd8cd05e1017353e6bfd635199f923d88
YakhiHabibi <anirudh16.ashok@gmail.com>
2026-02-20 10:30:10 +0530

    Remove welcome message comment from main method
    Removed commented code for clarity.

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -22,7 +22,6 @@ class PalindromeCheckerApp {
      * * @param args Command-line arguments
      **/
     public static void main(String[] args) {
-        // Displaying the welcome message
         System.out.println("Welcome to the Palindrome Checker Management System");
 	System.out.println("Version: 1.0");
         System.out.println("System initialized successfully");
